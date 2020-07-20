0c344a4904d732297b8f504a3453c92323cc47c1
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-07-20 18:12:44 -0300

    Primeiro commit Python

---
+++ primeirosComandos/hello.py
@@ -0,0 +1,8 @@
+#Testando o programa
+print('Hello, world!')
+
+print(7+4)
+
+print('7' + '4')
+
+print('Hello', 7 + 4)
---
+++ primeirosComandos/variáveis.py
@@ -0,0 +1,11 @@
+#Utilizando variáveis
+nome = 'Emerson'
+idade = 24
+peso = 69.8
+print(nome, idade, peso)
+
+#Utilizando variáveis interativas
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite seu peso: ')
+print(nome, idade, peso)
