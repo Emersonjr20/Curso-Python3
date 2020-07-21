77625dd86a9a51b1e6ad51a368c2fb78f026b158
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-07-21 02:22:46 -0300

    done

--- Python3/Challenges/challenge003.py
+++ Python3/Challenges/challenge003.py
@@ -1,3 +1,4 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
 print('Calcular soma entre dois números!')
 n1=int(input('Digite o primeiro: '))
 n2=int(input('Digite o segundo: '))
