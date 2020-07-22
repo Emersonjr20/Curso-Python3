5b4047f6b2abf62ccedaa83550dae85fd2157409
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-07-22 17:00:53 -0300

    rest

--- Python3/Exercises/ex012.py
+++ Python3/Exercises/ex012.py
@@ -1,4 +1,5 @@
+#Desconto de Produto
 print('='*3, 'TABELA DE DESCONTOS', '='*3)
 price = float(input('Digite o preço atual do produto: '))
-discount = price*0.95
+discount = price - (price*5/100)
 print('O valor desse produto com desconto de 5%, fica: R${:.2f}'.format(discount))
--- Python3/Exercises/ex013.py
+++ Python3/Exercises/ex013.py
@@ -1,5 +1,6 @@
+#Aumento de Salário
 print('='*3, 'Aumento de salário', '='*3)
 nome = str(input('Digite o nome do funcionário: '))
 s = float(input('Digite o salário atual de {}: '.format(nome)))
-a = s*1.15
+a = s + (s*15/100)
 print('O novo salário de {}, ficará: R${:.2f}'.format(nome, a))
