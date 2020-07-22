0f7bc7eae4c334b934b6c788aa0f2f4fb0f53e34
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-07-22 00:44:19 -0300

    Done

--- Python3/Exercises/ex008.py
+++ Python3/Exercises/ex008.py
@@ -1,5 +1,9 @@
 print('Conversor de medida:')
 n = float(input('Digite o valor: '))
+km = n/1000
+hm = n/100
+dam = n/10
+dm = n*10
 cm = n*100
-ml = n*1000
-print('O valor digitado é: {:.2f}m \nConversão para centímetros: {:.2f}cm \nConversão para milímetros: {:.2f}ml'.format(n, cm, ml))
+mm = n*1000
+print('O valor digitado é: {:.3f}m \nConversão para quilometros: {:.3f}km \nConversão para hectômetro: {:.3f}hm \nConversão para decâmetro: {:.3f}dam \nConversão para decímetros: {:.3f}dm \nConversão para centímetros: {:.3f}cm \nConversão para milímetros: {:.3f}mm'.format(n, km, hm, dam, dm, cm, mm))
