f9076ade1b7b53e738c1738ed5bbe05b198ae4c5
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-07-23 00:49:59 -0300

    rest

---
+++ Python3/Exercises/ex014.py
@@ -0,0 +1,4 @@
+print('='*3, 'CONVERSÃO DE TEMPERATURA', '='*3)
+c = int(input('Digite o valor de celsius: '))
+f = (c*1.8) + 32
+print('O valor {} de celsius convertido para fahrenheit é: {}'.format(c, f))
