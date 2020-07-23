ba930b793df7f44e327a6b4c61f514a5194aa290
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-07-23 11:13:11 -0300

    Start

---
+++ Python3/Challenges/challenge.py
@@ -0,0 +1,6 @@
+#016 Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.           Ex:Digite um número:6.127 tem a parte inteira 6.
+#017 Faça um programa que leia o comprimento do cateto oposto e o cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+#018 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+#019 Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+#020 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+#021 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
