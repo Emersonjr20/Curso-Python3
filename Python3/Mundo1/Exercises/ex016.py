4e5560146927ea4551af32b5a662252e0485e947
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-08-02 14:22:45 -0300

    correct exercises

--- Python3/Mundo1/Exercises/ex016.py
+++ Python3/Mundo1/Exercises/ex016.py
@@ -1,4 +1,7 @@
-from math import ceil
+'''from math import ceil
 n = float(input('Digite o número que deseja arredondar: '))
 i = ceil(n)
-print('O número digitado foi {}, e arredondado fica: {}'.format(n, i))
+print('O número digitado foi {}, e arredondado fica: {}'.format(n, i))'''
+
+n = float(input('Digite o número que deseja arredondar: '))
+print('O número digitado foi {}, e arredondado fica: {}'.format(n, int(n)))
---
+++ Python3/Mundo1/Exercises/ex019.py
@@ -0,0 +1,7 @@
+import random
+Emerson = 1
+Kaiuã = 2
+Brena = 3
+Gabi = 4
+num = random.randint(1, 4)
+print(num)
