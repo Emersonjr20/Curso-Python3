2ceb3c45b59af9085ae24959510c884c2a565549
Emerson Ribeiro <emerson.r.jr@hotmail.com>
2020-08-03 01:58:19 -0300

    draw

--- Python3/Mundo1/Exercises/ex019.py
+++ Python3/Mundo1/Exercises/ex019.py
@@ -1,7 +1,9 @@
-import random
-Emerson = 1
-Kaiuã = 2
-Brena = 3
-Gabi = 4
-num = random.randint(1, 4)
-print(num)
+from random import choice
+print('>'*3, 'Sorteio de alunos que irá apagar o Quadro hoje!', '<'*3)
+a1 = str(input('Primeiro aluno: '))
+a2 = str(input('Segundo aluno: '))
+a3 = str(input('Terceiro aluno: '))
+a4 = str(input('Quarto aluno: '))
+lista = [a1, a2, a3, a4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
